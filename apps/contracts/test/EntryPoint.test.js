c1e77bd153c0120a854bab834c6b6144a17c80a5
Hazim J <hazim.jumali@gmail.com>
2021-10-16 21:51:51 +1100

    cleanup

--- apps/contracts/test/EntryPoint.test.js
+++ apps/contracts/test/EntryPoint.test.js
@@ -9,8 +9,8 @@ const {
 describe("EntryPoint", () => {
   let create2Factory;
   let entryPoint;
-  let sender;
   let initCode;
+  let sender;
 
   beforeEach(async () => {
     const [SingletonFactory, EntryPoint, Wallet] = await Promise.all([
@@ -21,8 +21,8 @@ describe("EntryPoint", () => {
 
     create2Factory = await SingletonFactory.deploy();
     entryPoint = await EntryPoint.deploy(create2Factory.address);
-    sender = getWalletAddress(create2Factory.address, initCode);
     initCode = Wallet.getDeployTransaction().data;
+    sender = getWalletAddress(create2Factory.address, initCode);
   });
 
   describe("handleOps", () => {
