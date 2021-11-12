2e3d1c6ef5f1f38d35b7a2ce4d88fb892c8e6b3d
Hazim J <hazim.jumali@gmail.com>
2021-11-12 13:29:33 +1100

    Web app setup (#67)
    Co-authored-by: Nick Solante <nick.solante@catch.com.au>

---
+++ apps/web/pages/index.js
@@ -0,0 +1,15 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import { Register } from '../components/Register';
+import styles from '../styles/Home.module.css';
+import { ChakraProvider, Center } from '@chakra-ui/react';
+
+export default function Home() {
+  return (
+    <ChakraProvider>
+      <Center>
+        <Register />
+      </Center>
+    </ChakraProvider>
+  );
+}
