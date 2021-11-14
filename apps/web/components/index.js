d1642ae5e693dc26abc9195cf4b9b417c135bf08
Hazim J <hazim.jumali@gmail.com>
2021-11-14 18:54:01 +1100

    Add Home screen UI (#79)
    * Add Home screen UI
    
    * Add Home screen UI

--- apps/web/components/index.js
+++ apps/web/components/index.js
@@ -2,3 +2,4 @@ export * from './appContainer';
 export * from './head';
 export * from './header';
 export * from './inlineError';
+export * from './search';
---
+++ apps/web/pages/home.js
@@ -0,0 +1,42 @@
+import { Tabs, TabList, TabPanels, Tab, TabPanel } from '@chakra-ui/react';
+import { AppContainer, Head, Search } from '../components';
+
+export default function Home() {
+  return (
+    <>
+      <Head title="Stackup | Home" />
+
+      <div style={{ minHeight: '100vh' }}>
+        <Search />
+
+        <AppContainer minMargin>
+          <Tabs isFitted w="100%" variant="soft-rounded" colorScheme="blue" align="center">
+            <TabPanels>
+              <TabPanel>
+                <p>chats placeholder</p>
+              </TabPanel>
+              <TabPanel>
+                <p>Profile placeholder</p>
+              </TabPanel>
+            </TabPanels>
+
+            <TabList
+              borderWidth="1px"
+              borderTopRadius="lg"
+              borderBottomRadius={['0', 'lg']}
+              p="8px"
+              pos="absolute"
+              bottom={['0px', '32px']}
+              left={['0px', 'auto']}
+              maxW="xl"
+              w="100%"
+            >
+              <Tab borderRadius="lg">Chats</Tab>
+              <Tab borderRadius="lg">Profile</Tab>
+            </TabList>
+          </Tabs>
+        </AppContainer>
+      </div>
+    </>
+  );
+}
