768f05d67ffb64b5ad40daedd710e9b88f21034c
Hazim J <hazim.jumali@gmail.com>
2021-12-07 17:10:16 +1100

    Add deploy EntryPoint script

--- apps/contracts/lib/index.js
+++ apps/contracts/lib/index.js
@@ -1,2 +1,3 @@
 module.exports.constants = require("./constants");
+module.exports.contracts = require("./contracts");
 module.exports.wallet = require("./wallet");
---
+++ apps/contracts/scripts/deploy/entryPoint.js
@@ -0,0 +1,25 @@
+const { ethers } = require("hardhat");
+const { contracts } = require("../../lib");
+
+async function main() {
+  const [signer] = await ethers.getSigners();
+  const SingletonFactory = contracts.SingletonFactory.getContract(signer);
+
+  const tx = await SingletonFactory.deploy(
+    contracts.EntryPoint.deployInitCode,
+    contracts.EntryPoint.deploySalt,
+    {
+      gasLimit: 5000000,
+    }
+  ).then((tx) => tx.wait());
+
+  console.log("EntryPoint deployment transaction:", tx);
+  console.log("EntryPoint will be deployed to:", contracts.EntryPoint.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
