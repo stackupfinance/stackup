283d2bfdf0e9639e91062caa88fee5659f108f9b
Hazim J <hazim.jumali@gmail.com>
2021-12-28 13:05:39 +1100

    Complete recovery once majority of guardians have approved

--- apps/contracts/lib/constants/userOperations.js
+++ apps/contracts/lib/constants/userOperations.js
@@ -1,4 +1,4 @@
-module.exports.defaultGas = 210000;
+module.exports.defaultGas = 215000;
 module.exports.defaultMaxFee = 50000000000;
 module.exports.defaultMaxPriorityFee = this.defaultMaxFee;
 module.exports.initNonce = 0;
