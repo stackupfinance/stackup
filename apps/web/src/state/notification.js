d466ff1e1bb1c21350638081779a64c3ca926598
Hazim J <hazim.jumali@gmail.com>
2022-01-10 13:49:29 +1100

    Remove devtools (#203)

--- apps/web/src/state/notification.js
+++ apps/web/src/state/notification.js
@@ -1,5 +1,5 @@
 import create from 'zustand';
-import { persist, devtools } from 'zustand/middleware';
+import { persist } from 'zustand/middleware';
 import axios from 'axios';
 import { App } from '../config';
 
@@ -26,62 +26,60 @@ const defaultState = {
 };
 
 export const useNotificationStore = create(
-  devtools(
-    persist(
-      (set, get) => ({
-        ...defaultState,
+  persist(
+    (set, get) => ({
+      ...defaultState,
 
-        fetchNotifications: async (options) => {
-          set({ loading: true });
+      fetchNotifications: async (options) => {
+        set({ loading: true });
 
-          try {
-            const res = await axios.get(
-              `${App.stackup.backendUrl}/v1/users/${options.userId}/notifications`,
-              {
-                headers: { Authorization: `Bearer ${options.accessToken}` },
-              },
-            );
+        try {
+          const res = await axios.get(
+            `${App.stackup.backendUrl}/v1/users/${options.userId}/notifications`,
+            {
+              headers: { Authorization: `Bearer ${options.accessToken}` },
+            },
+          );
 
-            set({
-              loading: false,
-              notifications: res.data.notifications,
-            });
-          } catch (error) {
-            set({ loading: false });
-            throw error;
-          }
-        },
-
-        deleteNotification: async (notificationId, options) => {
-          set({ loading: true });
-
-          try {
-            const res = await axios.delete(
-              `${App.stackup.backendUrl}/v1/users/${options.userId}/notifications/${notificationId}`,
-              {
-                headers: { Authorization: `Bearer ${options.accessToken}` },
-              },
-            );
-
-            set({
-              loading: false,
-              notifications: res.data.notifications,
-            });
-          } catch (error) {
-            set({ loading: false });
-            throw error;
-          }
-        },
-
-        clear: () => set({ ...defaultState }),
-      }),
-      {
-        name: 'stackup-notification-store',
-        partialize: (state) => {
-          const { loading, ...persisted } = state;
-          return persisted;
-        },
+          set({
+            loading: false,
+            notifications: res.data.notifications,
+          });
+        } catch (error) {
+          set({ loading: false });
+          throw error;
+        }
       },
-    ),
+
+      deleteNotification: async (notificationId, options) => {
+        set({ loading: true });
+
+        try {
+          const res = await axios.delete(
+            `${App.stackup.backendUrl}/v1/users/${options.userId}/notifications/${notificationId}`,
+            {
+              headers: { Authorization: `Bearer ${options.accessToken}` },
+            },
+          );
+
+          set({
+            loading: false,
+            notifications: res.data.notifications,
+          });
+        } catch (error) {
+          set({ loading: false });
+          throw error;
+        }
+      },
+
+      clear: () => set({ ...defaultState }),
+    }),
+    {
+      name: 'stackup-notification-store',
+      partialize: (state) => {
+        const { loading, ...persisted } = state;
+        return persisted;
+      },
+    },
   ),
 );
--- apps/web/src/state/recover.js
+++ apps/web/src/state/recover.js
@@ -1,5 +1,5 @@
 import create from 'zustand';
-import { devtools, persist } from 'zustand/middleware';
+import { persist } from 'zustand/middleware';
 import axios from 'axios';
 import { nanoid } from 'nanoid';
 import { wallet, constants } from '@stackupfinance/contracts';
@@ -93,223 +93,221 @@ const paymasterApproval = async (userOperations) => {
 };
 
 export const useRecoverStore = create(
-  devtools(
-    persist(
-      (set, get) => ({
-        ...defaultState,
+  persist(
+    (set, get) => ({
+      ...defaultState,
 
-        createSignerAndUserOps: async (password) => {
-          const { user, guardians } = get();
-          if (!user || !guardians) return;
-          set({ loading: true });
+      createSignerAndUserOps: async (password) => {
+        const { user, guardians } = get();
+        if (!user || !guardians) return;
+        set({ loading: true });
 
-          try {
-            const { encryptedSigner } = wallet.proxy.initEncryptedIdentity(password);
-            const newOwner = wallet.proxy.decryptSigner({ encryptedSigner }, password).address;
-            const [isDeployed, allowance] = await Promise.all([
-              wallet.proxy.isCodeDeployed(provider, user.wallet.walletAddress),
-              usdcContract.allowance(user.wallet.walletAddress, App.web3.paymaster),
-            ]);
-            const shouldApprove = allowance.lte(defaultPaymasterReapproval);
-            const nonce = isDeployed
-              ? await wallet.proxy.getNonce(provider, user.wallet.walletAddress)
-              : constants.userOperations.initNonce;
-            const userOperations = await Promise.all([
-              shouldApprove
-                ? wallet.userOperations.get(user.wallet.walletAddress, {
-                    nonce,
-                    initCode: isDeployed
-                      ? constants.userOperations.nullCode
-                      : wallet.proxy.getInitCode(
-                          user.wallet.initImplementation,
-                          user.wallet.initEntryPoint,
-                          user.wallet.initOwner,
-                          user.wallet.initGuardians,
-                        ),
-                    callData: wallet.encodeFunctionData.ERC20Approve(
-                      App.web3.usdc,
-                      App.web3.paymaster,
-                      defaultPaymasterApproval,
-                    ),
-                  })
-                : undefined,
-              wallet.userOperations.get(user.wallet.walletAddress, {
-                nonce: shouldApprove ? nonce + 1 : nonce,
-                callData: wallet.encodeFunctionData.transferOwner(newOwner),
-              }),
-            ])
-              .then((ops) => ops.filter(Boolean))
-              .then(paymasterApproval);
+        try {
+          const { encryptedSigner } = wallet.proxy.initEncryptedIdentity(password);
+          const newOwner = wallet.proxy.decryptSigner({ encryptedSigner }, password).address;
+          const [isDeployed, allowance] = await Promise.all([
+            wallet.proxy.isCodeDeployed(provider, user.wallet.walletAddress),
+            usdcContract.allowance(user.wallet.walletAddress, App.web3.paymaster),
+          ]);
+          const shouldApprove = allowance.lte(defaultPaymasterReapproval);
+          const nonce = isDeployed
+            ? await wallet.proxy.getNonce(provider, user.wallet.walletAddress)
+            : constants.userOperations.initNonce;
+          const userOperations = await Promise.all([
+            shouldApprove
+              ? wallet.userOperations.get(user.wallet.walletAddress, {
+                  nonce,
+                  initCode: isDeployed
+                    ? constants.userOperations.nullCode
+                    : wallet.proxy.getInitCode(
+                        user.wallet.initImplementation,
+                        user.wallet.initEntryPoint,
+                        user.wallet.initOwner,
+                        user.wallet.initGuardians,
+                      ),
+                  callData: wallet.encodeFunctionData.ERC20Approve(
+                    App.web3.usdc,
+                    App.web3.paymaster,
+                    defaultPaymasterApproval,
+                  ),
+                })
+              : undefined,
+            wallet.userOperations.get(user.wallet.walletAddress, {
+              nonce: shouldApprove ? nonce + 1 : nonce,
+              callData: wallet.encodeFunctionData.transferOwner(newOwner),
+            }),
+          ])
+            .then((ops) => ops.filter(Boolean))
+            .then(paymasterApproval);
 
-            const channelId = nanoid(16);
-            const userGuardians = guardians.filter((g) => g !== App.web3.paymaster);
-            let status;
-            if (userGuardians.length) {
-              const res = await axios.post(
-                `${App.stackup.backendUrl}/v1/auth/recover/request-guardian-approval`,
-                {
-                  channelId,
-                  username: user.username,
-                  guardians: userGuardians,
-                  userOperations,
-                },
-              );
-              status = res.data.status;
-            }
-
-            set({ loading: false, encryptedSigner, userOperations, status, channelId });
-          } catch (error) {
-            set({ loading: false });
-            throw error;
+          const channelId = nanoid(16);
+          const userGuardians = guardians.filter((g) => g !== App.web3.paymaster);
+          let status;
+          if (userGuardians.length) {
+            const res = await axios.post(
+              `${App.stackup.backendUrl}/v1/auth/recover/request-guardian-approval`,
+              {
+                channelId,
+                username: user.username,
+                guardians: userGuardians,
+                userOperations,
+              },
+            );
+            status = res.data.status;
           }
-        },
 
-        lookup: async (data) => {
-          set({ loading: true });
+          set({ loading: false, encryptedSigner, userOperations, status, channelId });
+        } catch (error) {
+          set({ loading: false });
+          throw error;
+        }
+      },
 
-          try {
-            const res = await axios.post(`${App.stackup.backendUrl}/v1/auth/recover/lookup`, data);
+      lookup: async (data) => {
+        set({ loading: true });
 
-            const { user } = res.data;
-            const isDeployed =
-              (user.wallet?.walletAddress &&
-                (await wallet.proxy.isCodeDeployed(provider, user.wallet.walletAddress))) ||
-              false;
-            const guardians = isDeployed
-              ? await wallet.access.getGuardians(walletContract(user.wallet.walletAddress))
-              : user.wallet?.initGuardians ?? [];
+        try {
+          const res = await axios.post(`${App.stackup.backendUrl}/v1/auth/recover/lookup`, data);
 
-            const account = { user, guardians };
-            set({ loading: false, ...account });
-            return account;
-          } catch (error) {
-            set({ loading: false });
-            throw error;
-          }
-        },
+          const { user } = res.data;
+          const isDeployed =
+            (user.wallet?.walletAddress &&
+              (await wallet.proxy.isCodeDeployed(provider, user.wallet.walletAddress))) ||
+            false;
+          const guardians = isDeployed
+            ? await wallet.access.getGuardians(walletContract(user.wallet.walletAddress))
+            : user.wallet?.initGuardians ?? [];
 
-        sendVerificationEmail: async () => {
-          if (!get().user) return;
-          set({ loading: true });
+          const account = { user, guardians };
+          set({ loading: false, ...account });
+          return account;
+        } catch (error) {
+          set({ loading: false });
+          throw error;
+        }
+      },
 
-          try {
-            await axios.post(`${App.stackup.backendUrl}/v1/auth/recover/send-verification-email`, {
-              username: get().user.username,
-            });
+      sendVerificationEmail: async () => {
+        if (!get().user) return;
+        set({ loading: true });
 
-            set({ loading: false });
-          } catch (error) {
-            set({ loading: false });
-            throw error;
-          }
-        },
-
-        verifyEmail: async (code) => {
-          if (!get().user || !get().userOperations) return;
-          set({ loading: true });
-
-          try {
-            const res = await axios.post(`${App.stackup.backendUrl}/v1/auth/recover/verify-email`, {
-              username: get().user.username,
-              code,
-              userOperations: get().userOperations,
-            });
-
-            set({ loading: false, userOperations: res.data.userOperations });
-          } catch (error) {
-            set({ loading: false });
-            throw error;
-          }
-        },
-
-        updateStatus: (data) => {
-          const { userOperations, status } = get();
-          if (!userOperations || !status) return;
-
-          const updatedUserOps = userOperations.map((op, i) =>
-            wallet.userOperations.appendGuardianSignature(op, data.userOperations[i]),
-          );
-          const updatedStatus = status.map((s) => {
-            if (s.username === data.username) {
-              return { ...s, isComplete: true };
-            }
-            return s;
+        try {
+          await axios.post(`${App.stackup.backendUrl}/v1/auth/recover/send-verification-email`, {
+            username: get().user.username,
           });
-          set({ userOperations: updatedUserOps, status: updatedStatus });
-        },
 
-        confirm: async (password) => {
-          const { user, encryptedSigner, userOperations, channelId } = get();
-          if (!user || !encryptedSigner || !userOperations || !channelId) return;
-          set({ loading: true });
+          set({ loading: false });
+        } catch (error) {
+          set({ loading: false });
+          throw error;
+        }
+      },
 
-          try {
-            const signer = wallet.proxy.decryptSigner({ encryptedSigner }, password);
-            if (!signer) {
-              throw new Error('Incorrect password');
-            }
+      verifyEmail: async (code) => {
+        if (!get().user || !get().userOperations) return;
+        set({ loading: true });
 
-            await axios.post(`${App.stackup.backendUrl}/v1/auth/recover/confirm`, {
-              channelId,
-              username: user.username,
-              signature: await signer.signMessage(loginMessage),
-              encryptedSigner,
-              userOperations,
-            });
-          } catch (error) {
-            set({ loading: false });
-            throw error;
+        try {
+          const res = await axios.post(`${App.stackup.backendUrl}/v1/auth/recover/verify-email`, {
+            username: get().user.username,
+            code,
+            userOperations: get().userOperations,
+          });
+
+          set({ loading: false, userOperations: res.data.userOperations });
+        } catch (error) {
+          set({ loading: false });
+          throw error;
+        }
+      },
+
+      updateStatus: (data) => {
+        const { userOperations, status } = get();
+        if (!userOperations || !status) return;
+
+        const updatedUserOps = userOperations.map((op, i) =>
+          wallet.userOperations.appendGuardianSignature(op, data.userOperations[i]),
+        );
+        const updatedStatus = status.map((s) => {
+          if (s.username === data.username) {
+            return { ...s, isComplete: true };
           }
-        },
+          return s;
+        });
+        set({ userOperations: updatedUserOps, status: updatedStatus });
+      },
 
-        onComplete: async () => set({ loading: false }),
+      confirm: async (password) => {
+        const { user, encryptedSigner, userOperations, channelId } = get();
+        if (!user || !encryptedSigner || !userOperations || !channelId) return;
+        set({ loading: true });
 
-        selectGuardianRequest: (savedGuardianRequest) => set({ savedGuardianRequest }),
-
-        approveGuardianRequest: async (userWallet, password, options) => {
-          const { savedGuardianRequest } = get();
-          if (!savedGuardianRequest) return;
-
-          const signer = wallet.proxy.decryptSigner(userWallet, password);
+        try {
+          const signer = wallet.proxy.decryptSigner({ encryptedSigner }, password);
           if (!signer) {
             throw new Error('Incorrect password');
           }
-          set({ loading: true });
 
-          try {
-            const userOperations = await Promise.all(
-              savedGuardianRequest.userOperations.map((op) =>
-                wallet.userOperations.signAsGuardian(signer, userWallet.walletAddress, op),
-              ),
-            );
-
-            await axios.post(
-              `${App.stackup.backendUrl}/v1/auth/recover/send-guardian-approval`,
-              {
-                channelId: savedGuardianRequest.channelId,
-                userOperations,
-              },
-              {
-                headers: { Authorization: `Bearer ${options.accessToken}` },
-              },
-            );
-
-            set({ loading: false });
-          } catch (error) {
-            set({ loading: false });
-            throw error;
-          }
-        },
-
-        clear: () => set({ ...defaultState }),
-      }),
-      {
-        name: 'stackup-recover-store',
-        partialize: (state) => {
-          const { loading, ...persisted } = state;
-          return persisted;
-        },
+          await axios.post(`${App.stackup.backendUrl}/v1/auth/recover/confirm`, {
+            channelId,
+            username: user.username,
+            signature: await signer.signMessage(loginMessage),
+            encryptedSigner,
+            userOperations,
+          });
+        } catch (error) {
+          set({ loading: false });
+          throw error;
+        }
       },
-    ),
+
+      onComplete: async () => set({ loading: false }),
+
+      selectGuardianRequest: (savedGuardianRequest) => set({ savedGuardianRequest }),
+
+      approveGuardianRequest: async (userWallet, password, options) => {
+        const { savedGuardianRequest } = get();
+        if (!savedGuardianRequest) return;
+
+        const signer = wallet.proxy.decryptSigner(userWallet, password);
+        if (!signer) {
+          throw new Error('Incorrect password');
+        }
+        set({ loading: true });
+
+        try {
+          const userOperations = await Promise.all(
+            savedGuardianRequest.userOperations.map((op) =>
+              wallet.userOperations.signAsGuardian(signer, userWallet.walletAddress, op),
+            ),
+          );
+
+          await axios.post(
+            `${App.stackup.backendUrl}/v1/auth/recover/send-guardian-approval`,
+            {
+              channelId: savedGuardianRequest.channelId,
+              userOperations,
+            },
+            {
+              headers: { Authorization: `Bearer ${options.accessToken}` },
+            },
+          );
+
+          set({ loading: false });
+        } catch (error) {
+          set({ loading: false });
+          throw error;
+        }
+      },
+
+      clear: () => set({ ...defaultState }),
+    }),
+    {
+      name: 'stackup-recover-store',
+      partialize: (state) => {
+        const { loading, ...persisted } = state;
+        return persisted;
+      },
+    },
   ),
 );
