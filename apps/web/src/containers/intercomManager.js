4095a6b9733007ee7978e48fc05679bed557de37
Hazim J <hazim.jumali@gmail.com>
2022-02-24 23:08:45 -0800

    cleanup

--- apps/web/src/containers/intercomManager.js
+++ apps/web/src/containers/intercomManager.js
@@ -1,13 +1,18 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useIntercom } from 'react-use-intercom';
 import { useAccountStore, accountIntercomManagerSelector } from '../state';
 
 export const IntercomManager = ({ children }) => {
   const { accessToken, user } = useAccountStore(accountIntercomManagerSelector);
   const { boot, update, shutdown } = useIntercom();
+  const [shouldBoot, setShouldBoot] = useState(true);
 
   useEffect(() => {
     if (accessToken) {
+      if (shouldBoot) {
+        boot();
+        setShouldBoot(false);
+      }
       update({
         name: user.username,
         user_id: user.id,
