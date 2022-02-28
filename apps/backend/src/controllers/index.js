ebecf107a74a5e6f34f1d397e36a9718db3415b3
Hazim J <hazim.jumali@gmail.com>
2022-02-27 16:59:47 -0800

    cleanup

--- apps/backend/src/controllers/index.js
+++ apps/backend/src/controllers/index.js
@@ -1,3 +1,3 @@
 module.exports.authController = require('./auth.controller');
-module.exports.userController = require('./user.controller');
 module.exports.inviteController = require('./invite.controller');
+module.exports.userController = require('./user.controller');
