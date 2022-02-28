af55129f0987f05aa6d7a9c2df91a792fc85431a
Hazim J <hazim.jumali@gmail.com>
2022-02-27 16:58:59 -0800

    cleanup

--- apps/backend/src/models/index.js
+++ apps/backend/src/models/index.js
@@ -1,7 +1,7 @@
 module.exports.Code = require('./code.model');
+module.exports.Invite = require('./invite.model');
 module.exports.Notification = require('./notification.model');
 module.exports.Token = require('./token.model');
 module.exports.Transaction = require('./transaction.model');
 module.exports.User = require('./user.model');
 module.exports.Wallet = require('./wallet.model');
-module.exports.Invite = require('./invite.model');
