9bb2c8aa7c076bbf325630baecdc2d6511700ace
Hazim J <hazim.jumali@gmail.com>
2022-03-03 13:59:13 -0800

    Add Wyre checkout integration

--- apps/web/src/hooks/auth.js
+++ apps/web/src/hooks/auth.js
@@ -82,7 +82,6 @@ export const useAuth = () => {
 
   const isLoggedOut = () => !refreshToken;
   const refreshTokenExpired = () => isExpired(refreshToken?.token);
-  const accessTokenExpired = () => isExpired(accessToken?.token);
   const notOnAuthPage = () => !initAuthRoutes.has(location.pathname);
   const onLoginPage = () => location.pathname === Routes.LOGIN;
   const shouldRefresh = () => accessToken && refreshToken && !isExpired(refreshToken.token);
@@ -99,7 +98,6 @@ export const useAuth = () => {
           setIsFirst(false);
           await refresh();
         } else {
-          accessTokenExpired() && (await refresh());
           onLoginPage() && router.push(Routes.HOME);
         }
       } catch (error) {
@@ -116,9 +114,12 @@ export const useAuth = () => {
   }, [refreshToken]);
 
   useEffect(() => {
-    const interval = setInterval(() => {
+    const interval = setInterval(async () => {
       if (shouldRefresh()) {
-        refresh();
+        // JWTs could have been updated in other tabs.
+        // Should manually rehydrate from storage here before refresh.
+        await useAccountStore.persist.rehydrate();
+        await refresh();
       }
     }, REFRESH_INTERVAL_MS);
 
