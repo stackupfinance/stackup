cb576d361670016722a6acb5d097ebdf885fc67f
Hazim J <hazim.jumali@gmail.com>
2022-03-18 10:33:06 +1100

    Retry parseBlock if receipts not found

--- apps/backend/src/jobs/parseBlock.jobs.js
+++ apps/backend/src/jobs/parseBlock.jobs.js
@@ -8,12 +8,17 @@ const log = (msg) => logger.info(`JOB ${types.parseBlock}: ${msg}`);
 const parseBlock = (queue) => {
   queue.define(types.parseBlock, async (job) => {
     try {
-      const { chainId, blockNumber } = job.attrs.data;
+      const { chainId, blockNumber, attempt = 0 } = job.attrs.data;
       const receipts = await alchemyService.getTransactionReceipts(chainId, blockNumber);
-      const transactions = transactionService.parseReceiptsForIncomingTransfers(chainId, receipts);
-
-      await transactionService.indexIncomingTransfers(transactions);
-      log(`blockNumber: ${blockNumber}, incomingTransfers: ${transactions.length}`);
+      if (!receipts && attempt <= 1) {
+        queue.schedule('1 seconds', types.parseBlock, { ...job.attrs.data, attempt: attempt + 1 });
+      } else if (!receipts) {
+        throw new Error('receipts not found');
+      } else {
+        const transactions = transactionService.parseReceiptsForIncomingTransfers(chainId, receipts);
+        await transactionService.indexIncomingTransfers(transactions);
+        log(`blockNumber: ${blockNumber}, incomingTransfers: ${transactions.length}`);
+      }
     } catch (error) {
       logger.error(error);
       throw error;
