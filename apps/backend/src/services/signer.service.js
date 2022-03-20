4b1aad18072fb9b2d77cd114449d81249fc74703
Hazim J <hazim.jumali@gmail.com>
2022-03-20 22:02:48 +1100

    Patch for approving fee token on non paymaster bug (#370)

--- apps/backend/src/services/signer.service.js
+++ apps/backend/src/services/signer.service.js
@@ -2,16 +2,32 @@ const { ethers } = require('ethers');
 const { wallet, contracts, constants } = require('@stackupfinance/contracts');
 const { web3 } = require('../config/config');
 const { signer, defaultPaymasterFee } = require('../utils/web3');
+const { functionSignatures } = require('../config/transaction');
 
 const signUserOpAsGuardian = async (userOperations) => {
   return Promise.all(userOperations.map((op) => wallet.userOperations.signAsGuardian(signer, web3.paymaster, op)));
 };
 
 const signUserOpWithPaymaster = async (userOperation, index) => {
+  // TODO: Fix this in the wallet smart contract. This is a bypass for alpha release.
+  const isApprovingFeeTokenOnNonPaymaster = (userOp) => {
+    const opcd = wallet.decodeCallData.fromUserOperation(userOp);
+    const wcd = wallet.decodeCallData.Erc20FromExecuteUserOp(opcd);
+    if (!wcd) return false;
+
+    const isApprove = wcd.signature === functionSignatures.erc20Approve;
+    const isFeeToken = opcd.args[0] === web3.usdc;
+    const isNotPaymaster = wcd.args[0] !== web3.paymaster;
+    if (isApprove && isFeeToken && isNotPaymaster) {
+      return true;
+    }
+    return false;
+  };
+
   return wallet.userOperations.signPaymasterData(
     signer,
     web3.paymaster,
-    index > 0 ? 0 : defaultPaymasterFee,
+    index > 0 || isApprovingFeeTokenOnNonPaymaster(userOperation) ? 0 : defaultPaymasterFee,
     web3.usdc,
     web3.usdcPriceFeed,
     userOperation
