88d706ba5277a18289c665f7d8b3ffd85471935c
Hazim J <hazim.jumali@gmail.com>
2022-03-20 19:19:45 +1100

    Perform null check on address before truncating (#369)

--- apps/backend/src/utils/web3.js
+++ apps/backend/src/utils/web3.js
@@ -96,6 +96,8 @@ module.exports.signatureCount = (userOp) => {
 };
 
 module.exports.truncateAddress = (address) => {
+  if (!address) return;
+
   const match = address.match(/^(0x[a-zA-Z0-9]{3})[a-zA-Z0-9]+([a-zA-Z0-9]{4})$/);
   if (!match) return address;
 
