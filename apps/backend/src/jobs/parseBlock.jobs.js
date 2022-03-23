ee6bc6719e3f7a68211ac54498980120c73a2a44
Hazim J <hazim.jumali@gmail.com>
2022-03-24 10:32:34 +1100

    Edit exp backoff comment

--- apps/backend/src/jobs/parseBlock.jobs.js
+++ apps/backend/src/jobs/parseBlock.jobs.js
@@ -8,8 +8,8 @@ const MAX_ATTEMPTS = 10;
 const MAX_DELAY = 600;
 const BASE_DELAY = 5;
 const exponentialBackoffDelay = (attempt) => {
-  // This function returns a delay value that starts at 5 seconds
-  // and exponentially increases to a cap of 600 seconds with full jitter.
+  // This function returns a delay value that starts at $BASE_DELAY seconds
+  // and exponentially increases to a cap of $MAX_DELAY seconds with full jitter.
   return `${Math.round(Math.random() * Math.min(MAX_DELAY, BASE_DELAY * 2 ** attempt))} seconds`;
 };
 const log = (msg) => logger.info(`JOB ${types.parseBlock}: ${msg}`);
