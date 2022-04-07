5d01b030f18f4f294dc148604cbaf509956c1842
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2022-04-07 19:09:42 -0300

    contracts: entry point inline main functions

--- apps/contracts/contracts/entrypoint/EntryPointHelpers.sol
+++ apps/contracts/contracts/entrypoint/EntryPointHelpers.sol
@@ -5,13 +5,7 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
 
-import "./Staking.sol";
-import "./EntryPoint.sol";
-import "./ISingletonFactory.sol";
 import "../UserOperation.sol";
-import "../helpers/Calls.sol";
-import "../wallet/IWallet.sol";
-import "../paymaster/IPaymaster.sol";
 
 library EntryPointHelpers {
   using Address for address;
@@ -19,21 +13,35 @@ library EntryPointHelpers {
   /**
    * @dev Tells whether the op has requested a paymaster or not
    */
-  function hasPaymaster(UserOperation memory op) internal pure returns (bool) {
+  function hasPaymaster(UserOperation calldata op) internal pure returns (bool) {
     return op.paymaster != address(0);
+  }
+
+  /**
+   * @dev Tells whether the op has an init code set or not
+   */
+  function hasInitCode(UserOperation calldata op) internal pure returns (bool) {
+    return op.initCode.length != 0;
+  }
+
+  /**
+   * @dev Tells whether the op wallet was already deployed or not
+   */
+  function isAlreadyDeployed(UserOperation calldata op) internal view returns (bool) {
+    return op.sender.isContract();
   }
 
   /**
    * @dev Tells the entry point request ID: op + entry point + chain ID
    */
-  function requestId(UserOperation memory op) internal view returns (bytes32) {
+  function requestId(UserOperation calldata op) internal view returns (bytes32) {
     return keccak256(abi.encode(hash(op), address(this), block.chainid));
   }
 
   /**
    * @dev Tells the total amount in wei that must be refunded to the entry point for a given op
    */
-  function requiredPrefund(UserOperation memory op) internal view returns (uint256) {
+  function requiredPrefund(UserOperation calldata op) internal view returns (uint256) {
     uint256 totalGas = op.callGas + op.verificationGas + op.preVerificationGas;
     return totalGas * gasPrice(op);
   }
@@ -41,7 +49,7 @@ library EntryPointHelpers {
   /**
    * @dev Tells the gas price to be used for an op. It uses GASPRICE for chains that don't support EIP1559 transactions.
    */
-  function gasPrice(UserOperation memory op) internal view returns (uint256) {
+  function gasPrice(UserOperation calldata op) internal view returns (uint256) {
     return
       op.maxFeePerGas == op.maxPriorityFeePerGas
         ? op.maxFeePerGas
@@ -49,44 +57,9 @@ library EntryPointHelpers {
   }
 
   /**
-   * @dev It deploys the wallet contract if necessary
-   */
-  function deployWalletIfNecessary(UserOperation memory op, ISingletonFactory create2Factory) internal {
-    bool hasInitCode = op.initCode.length != 0;
-    bool isAlreadyDeployed = op.sender.isContract();
-    require((isAlreadyDeployed && !hasInitCode) || (!isAlreadyDeployed && hasInitCode), "EntryPoint: Wrong init code");
-    if (!isAlreadyDeployed) create2Factory.deploy(op.initCode, bytes32(op.nonce));
-  }
-
-  /**
-   * @dev It validates the paymaster stake and calls `IPaymaster#validatePaymasterUserOp` if necessary
-   */
-  function validatePaymasterIfNecessary(UserOperation memory op, Staking.Stake storage stake)
-    internal
-    returns (bytes memory)
-  {
-    if (!hasPaymaster(op)) return new bytes(0);
-    require(stake.isLocked, "EntryPoint: Stake not locked");
-    require(stake.value >= requiredPrefund(op), "EntryPoint: Insufficient stake");
-    stake.value = stake.value - requiredPrefund(op);
-    return IPaymaster(op.paymaster).validatePaymasterUserOp(op, requiredPrefund(op));
-  }
-
-  /**
-   * @dev It validates a user operation calling `IWallet#validateUserOp`
-   */
-  function validateUserOp(UserOperation memory op) internal {
-    uint256 prefundRequired = hasPaymaster(op) ? 0 : requiredPrefund(op);
-    uint256 initBalance = address(this).balance;
-    IWallet(op.sender).validateUserOp{ gas: op.verificationGas }(op, requestId(op), prefundRequired);
-    uint256 actualPrefund = address(this).balance - initBalance;
-    require(actualPrefund >= prefundRequired, "EntryPoint: incorrect prefund");
-  }
-
-  /**
    * @dev Hashes a user operation
    */
-  function hash(UserOperation memory op) internal pure returns (bytes32) {
+  function hash(UserOperation calldata op) internal pure returns (bytes32) {
     return
       keccak256(
         abi.encodePacked(
