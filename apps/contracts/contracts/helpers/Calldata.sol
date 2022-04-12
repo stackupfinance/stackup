5caba245aa0f9dd7b8b6a89b5bc920150e253ec4
Facu Spagnuolo <facuspagnuolo@users.noreply.github.com>
2022-04-12 12:49:06 -0300

    Contracts: Improve safe math (#452)

--- apps/contracts/contracts/helpers/Calldata.sol
+++ apps/contracts/contracts/helpers/Calldata.sol
@@ -2,6 +2,7 @@
 
 pragma solidity ^0.8.0;
 
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import "solidity-bytes-utils/contracts/BytesLib.sol";
 
@@ -12,6 +13,8 @@ import "../wallet/IWallet.sol";
  * @dev Calldata helpers library
  */
 library Calldata {
+  using SafeMath for uint256;
+
   function isExecuteUserOp(bytes memory self) internal pure returns (bool) {
     return selector(self) == IWallet.executeUserOp.selector;
   }
@@ -29,6 +32,6 @@ library Calldata {
   }
 
   function params(bytes memory self) internal pure returns (bytes memory) {
-    return BytesLib.slice(self, 4, self.length - 4);
+    return BytesLib.slice(self, 4, self.length.sub(4, "Calldata: No params to decode"));
   }
 }
