c6fd8bf1d6bd1661833713c97702b7d568592d18
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2022-04-12 13:09:23 -0300

    contracts: split wallet and paymaster tests

---
+++ apps/contracts/test/utils/models/paymaster/PaymasterDeployer.ts
@@ -0,0 +1,22 @@
+import { getSigner } from '../../helpers/signers'
+import { deploy, instanceAt } from '../../helpers/contracts'
+import { encodePaymasterInit } from '../../helpers/encoding'
+
+import Paymaster from './Paymaster'
+import { PaymasterDeployParams } from '../../types'
+
+const PaymasterDeployer = {
+  async deploy(params: PaymasterDeployParams): Promise<Paymaster> {
+    const entryPoint = params.entryPoint ?? (await deploy('EntryPointMock'))
+    const implementation = await deploy('Paymaster')
+    const owner = params.owner ?? await getSigner()
+    const initialization = await encodePaymasterInit(entryPoint, owner, params.guardians)
+
+    const proxy = await deploy('ERC1967Proxy', [implementation.address, initialization])
+    const instance = await instanceAt('Paymaster', proxy.address)
+    await entryPoint.setWallet(instance.address)
+    return new Paymaster(instance, implementation, entryPoint, owner, params.guardians || [])
+  }
+}
+
+export default PaymasterDeployer
