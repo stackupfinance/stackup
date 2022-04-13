a56c46a7ca82be2097071ca3aba85a9c2fdd4dd6
arbob <arbob@live.in>
2022-04-13 11:17:09 +0530

    cleanup

--- apps/web/src/utils/openReplay.js
+++ apps/web/src/utils/openReplay.js
@@ -3,6 +3,6 @@ import { App } from '../config';
 
 export const openReplayTracker = new Tracker({
   projectKey: App.openReplay.projectKey,
-  __DISABLE_SECURE_MODE: process.env.NODE_ENV === 'production' ? false : true,
+  __DISABLE_SECURE_MODE: process.env.NODE_ENV === 'production',
   onStart: ({ sessionID }) => console.log('OpenReplay tracker started with session: ', sessionID),
 });
