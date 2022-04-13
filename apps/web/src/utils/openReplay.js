2a7f85d920a4ed8bd376fcd2da4e39e1b0cdcc63
arbob <arbob@live.in>
2022-04-13 15:41:59 +0530

    enabled input obscured

--- apps/web/src/utils/openReplay.js
+++ apps/web/src/utils/openReplay.js
@@ -4,4 +4,6 @@ import { App } from '../config';
 export const openReplayTracker = new Tracker({
   projectKey: App.openReplay.projectKey,
   __DISABLE_SECURE_MODE: process.env.NODE_ENV !== 'production',
+  defaultInputMode: 1, // 0 = Plain, 1 = Obscured, 2 = Ignored
+  onStart: ({ sessionID }) => console.log('OpenReplay tracker started with session: ', sessionID),
 });
