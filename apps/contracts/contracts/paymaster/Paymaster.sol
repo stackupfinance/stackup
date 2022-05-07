765138b1eee8cec5398b4c09763812aed32af7b6
Facu Spagnuolo <facundo_spagnuolo@icloud.com>
2022-05-07 11:17:48 -0300

    contracts: pin solidity version to 0.8.9

--- apps/contracts/contracts/paymaster/Paymaster.sol
+++ apps/contracts/contracts/paymaster/Paymaster.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: AGPL-3.0
 
-pragma solidity ^0.8.0;
+pragma solidity 0.8.9;
 
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
