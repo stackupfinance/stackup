d3097a615faa81a8f05a8aa4a26f94cd6bc11f55
Hazim J <hazim.jumali@gmail.com>
2022-05-09 23:03:07 +1000

    Icon integration with FontAwesome

--- apps/mobile/src/screens/home/earn.tsx
+++ apps/mobile/src/screens/home/earn.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import {Box, Text} from 'native-base';
-import type {BottomTabScreenProps} from '@react-navigation/bottom-tabs';
+import type {MaterialTopTabScreenProps} from '@react-navigation/material-top-tabs';
 import {HomeTabParamList} from '../../config';
 
-type Props = BottomTabScreenProps<HomeTabParamList, 'Earn'>;
+type Props = MaterialTopTabScreenProps<HomeTabParamList, 'Earn'>;
 
 export default function EarnScreen({}: Props) {
   return (
