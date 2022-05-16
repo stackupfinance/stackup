5ef6838a49af787fb31fd8f105eb606541bba8fb
Hazim J <hazim.jumali@gmail.com>
2022-05-16 23:06:41 +1000

    Add Sentry integration for mobile (#601)

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,8 +1,13 @@
-import * as React from 'react';
+import React, {useRef, MutableRefObject} from 'react';
 import {StatusBar} from 'react-native';
 import {NativeBaseProvider} from 'native-base';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  NavigationContainerRef,
+} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import * as Sentry from '@sentry/react-native';
+import {STACKUP_MOBILE_SENTRY_DNS} from '@env';
 import {
   HomeScreen,
   SecurityScreen,
@@ -18,21 +23,35 @@ import {
 import {useAuth} from './src/hooks';
 import {useNavigationStoreAppSelector} from './src/state';
 
+const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
+Sentry.init({
+  dsn: STACKUP_MOBILE_SENTRY_DNS,
+  tracesSampleRate: 0.5,
+  integrations: [new Sentry.ReactNativeTracing({routingInstrumentation})],
+});
+
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function App() {
   const {isReady, hasWalletInstance} = useAuth();
   const {initialNavigationState, setInitialNavigationState} =
     useNavigationStoreAppSelector();
+  const navigation = useRef() as MutableRefObject<
+    NavigationContainerRef<RootStackParamList>
+  >;
 
   return (
     <NativeBaseProvider theme={NativeBaseTheme}>
       <StatusBar barStyle="light-content" />
       {isReady ? (
         <NavigationContainer
+          ref={navigation}
           theme={NavigationTheme}
           initialState={initialNavigationState}
-          onStateChange={setInitialNavigationState}>
+          onStateChange={setInitialNavigationState}
+          onReady={() => {
+            routingInstrumentation.registerNavigationContainer(navigation);
+          }}>
           <Stack.Navigator screenOptions={{headerShown: false}}>
             {hasWalletInstance ? (
               <>
@@ -54,4 +73,4 @@ function App() {
   );
 }
 
-export default App;
+export default Sentry.wrap(App);
--- apps/mobile/scripts/buildenv.sh
+++ apps/mobile/scripts/buildenv.sh
@@ -5,7 +5,15 @@
 
 echo \
 "STACKUP_MOBILE_BACKEND_URL=${STACKUP_MOBILE_BACKEND_URL:-http://localhost:3000}
-STACKUP_MOBILE_AMPLITUDE_API_KEY=${STACKUP_MOBILE_AMPLITUDE_API_KEY}" \
+STACKUP_MOBILE_AMPLITUDE_API_KEY=${STACKUP_MOBILE_AMPLITUDE_API_KEY}
+STACKUP_MOBILE_SENTRY_DNS=${STACKUP_MOBILE_SENTRY_DNS}" \
 > .env
 
-echo ".env file successfully generated."
+echo \
+"defaults.url=https://sentry.io/
+defaults.org=stackup
+defaults.project=stackup-mobile
+auth.token=${STACKUP_MOBILE_SENTRY_AUTH_TOKEN}" \
+| tee ./ios/sentry.properties ./android/sentry.properties
+
+echo "Env files successfully generated."
