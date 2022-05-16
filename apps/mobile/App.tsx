edf72c75fb540b77910c114ccc72516e4affa0a5
Hazim J <hazim.jumali@gmail.com>
2022-05-16 22:58:07 +1000

    Add Sentry integration for mobile

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,8 +1,12 @@
-import * as React from 'react';
+import React, {useRef, MutableRefObject} from 'react';
 import {StatusBar} from 'react-native';
 import {NativeBaseProvider} from 'native-base';
-import {NavigationContainer} from '@react-navigation/native';
+import {
+  NavigationContainer,
+  NavigationContainerRef,
+} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import * as Sentry from '@sentry/react-native';
 import {
   HomeScreen,
   SecurityScreen,
@@ -17,6 +21,14 @@ import {
 } from './src/config';
 import {useAuth} from './src/hooks';
 import {useNavigationStoreAppSelector} from './src/state';
+import {STACKUP_MOBILE_SENTRY_DNS} from '@env';
+
+const routingInstrumentation = new Sentry.ReactNavigationInstrumentation();
+Sentry.init({
+  dsn: STACKUP_MOBILE_SENTRY_DNS,
+  tracesSampleRate: 0.5,
+  integrations: [new Sentry.ReactNativeTracing({routingInstrumentation})],
+});
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
@@ -24,15 +36,22 @@ function App() {
   const {isReady, hasWalletInstance} = useAuth();
   const {initialNavigationState, setInitialNavigationState} =
     useNavigationStoreAppSelector();
+  const navigation = useRef() as MutableRefObject<
+    NavigationContainerRef<RootStackParamList>
+  >;
 
   return (
     <NativeBaseProvider theme={NativeBaseTheme}>
       <StatusBar barStyle="light-content" />
       {isReady ? (
         <NavigationContainer
+          ref={navigation}
           theme={NavigationTheme}
           initialState={initialNavigationState}
-          onStateChange={setInitialNavigationState}>
+          onStateChange={setInitialNavigationState}
+          onReady={() => {
+            routingInstrumentation.registerNavigationContainer(navigation);
+          }}>
           <Stack.Navigator screenOptions={{headerShown: false}}>
             {hasWalletInstance ? (
               <>
@@ -54,4 +73,4 @@ function App() {
   );
 }
 
-export default App;
+export default Sentry.wrap(App);
