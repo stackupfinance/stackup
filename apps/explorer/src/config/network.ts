885407348b416c37343e33db957e9f482c4d386a
Hazim J <hazim.jumali@gmail.com>
2022-06-09 01:50:29 +1000

    Use NETWORK_ENV instead of NODE_ENV (#693)

--- apps/explorer/src/config/network.ts
+++ apps/explorer/src/config/network.ts
@@ -18,13 +18,13 @@ export const NetworksConfig: Record<Networks, NetworksConfig> = {
       MATIC: { address: ethers.constants.AddressZero },
       USDC: {
         address:
-          Env.NODE_ENV === "production"
+          Env.NETWORK_ENV === "mainnet"
             ? "0x2791Bca1f2de4661ED88A30C99A7a9449Aa84174"
             : "0x2058A9D7613eEE744279e3856Ef0eAda5FCbaA7e",
       },
       ETH: {
         address:
-          Env.NODE_ENV === "production"
+          Env.NETWORK_ENV === "mainnet"
             ? "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619"
             : "0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa",
       },
