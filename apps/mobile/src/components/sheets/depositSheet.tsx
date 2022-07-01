ff01a5af4fa8f1f4ff90d46f94af4b10af3163d1
Jonathan Spruance <jspruance@inflightcorp.com>
2022-07-01 11:44:45 -0400

    Ramp Network integration with updated SDK

--- apps/mobile/src/components/sheets/depositSheet.tsx
+++ apps/mobile/src/components/sheets/depositSheet.tsx
@@ -3,6 +3,7 @@ import {VStack} from 'native-base';
 import {faArrowRight} from '@fortawesome/free-solid-svg-icons/faArrowRight';
 import {BaseSheet} from '.';
 import {MenuItem} from '..';
+import RampSdk from '@ramp-network/react-native-sdk';
 
 type Props = {
   isOpen: boolean;
@@ -15,6 +16,13 @@ export const DepositSheet = ({
   onTransferFromWalletPress,
   onClose,
 }: Props) => {
+  const ramp = new RampSdk({
+    url: 'https://ri-widget-staging.firebaseapp.com',
+    hostAppName: 'Stackup',
+    hostLogoUrl: 'https://rampnetwork.github.io/assets/misc/test-logo.png',
+  }).on('*', event => {
+    console.log(`RampSdk.on('*')`, event);
+  });
   return (
     <BaseSheet title="Deposit" isOpen={isOpen} onClose={onClose}>
       <VStack flex={1} p="24px" backgroundColor="background.1" space="11px">
@@ -22,6 +30,11 @@ export const DepositSheet = ({
           heading="Transfer from wallet"
           icon={faArrowRight}
           onPress={onTransferFromWalletPress}
+        />
+        <MenuItem
+          heading="Deposit with Ramp"
+          icon={faArrowRight}
+          onPress={() => ramp?.show()}
         />
       </VStack>
     </BaseSheet>
