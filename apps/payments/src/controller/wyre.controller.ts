21dd558811df3df78c121aa55bc53db158cc956a
Jonathan Spruance <jspruance@inflightcorp.com>
2022-07-22 16:44:10 -0700

    Adding new wyre routes, vars

--- apps/payments/src/controller/wyre.controller.ts
+++ apps/payments/src/controller/wyre.controller.ts
@@ -16,8 +16,6 @@ export const attachAddress = async (
   res: Response,
   next: NextFunction
 ) => {
-  const response: PostResponse = {};
-
   try {
     const attachAddressResponse = await axios.post(
       "https://api.testwyre.com/v2/paymentMethod/PA_PTXN82CCWRH/attach"
@@ -26,6 +24,71 @@ export const attachAddress = async (
   } catch (error: any) {
     // handle error
     logger.error(`Attach address request error: ${error.message}`);
+    next(error);
+  }
+};
+
+export const createPaymentMethod = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const options = {
+      headers: {
+        'content-type': 'text/json',
+        'Authorization': `Bearer ${process.env.WYRE_API_SECRET}`
+      }
+    };
+    console.log(`processor token in createPaymentMethod :::: ${req.body.processor_token} Wyre secret :::: ${process.env.WYRE_API_SECRET}`);
+    const requestData = {
+      "plaidProcessorToken": req.body.processor_token,
+      "paymentMethodType": "LOCAL_TRANSFER",
+      "country": "US"
+    };
+    const createPaymentMethodResponse = await axios.post(
+      `https://api.testwyre.com/v2/paymentMethods?masqueradeAs=user:${req.body.wyre_user_id}`,
+      requestData,
+      options
+    );
+    res.json(createPaymentMethodResponse);
+  } catch (error: any) {
+    // handle error
+    logger.error(`Create payment method request error: ${error.message}`);
+    next(error);
+  }
+};
+
+export const createUser = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const options = {
+      headers: {
+        'Accept': 'text/json',
+        'Authorization': `Bearer null`,
+        'content-type': 'text/json',
+      }
+    };
+    
+    const requestData = {
+      "blockchains": [
+           "[]"
+      ],
+      "immediate": false
+    };
+
+    const createUserResponse = await axios.post(
+      "https://api.testwyre.com/v3/users",
+      requestData,
+      options
+    );
+    res.json(createUserResponse);
+  } catch (error: any) {
+    // handle error
+    logger.error(`Create user request error: ${error.message}`);
     next(error);
   }
 };
--- apps/payments/src/routes/v1/wyre.route.ts
+++ apps/payments/src/routes/v1/wyre.route.ts
@@ -4,6 +4,8 @@ import * as wyreController from "../../controller/wyre.controller";
 const router = express.Router();
 
 router.route("/health").get(wyreController.health);
+router.route("/create_user").post(wyreController.createUser);
+router.route("/create_payment_method").post(wyreController.createPaymentMethod);
 router.route("/attach_address").post(wyreController.attachAddress);
 
 export default router;
